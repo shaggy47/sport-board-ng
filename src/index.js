ec58fd7d9a8a28128dffc1897b5918055a79cd27
vaibhavpfatkal <vaibhavpfatkal@gmail.com>
2018-03-17 18:10:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+app.use(express.static(path.resolve(__dirname,"../dist/")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3500);
