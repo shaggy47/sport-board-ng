b3938b941699da211891895f618370f5bc7d908f
vaibhavpfatkal <vaibhavpfatkal@gmail.com>
2018-03-25 14:45:01 +0530

    schema for user api

--- sport-board/src/app-server/dbUtil.js
+++ sport-board/src/app-server/dbUtil.js
@@ -1,5 +1,14 @@
 var mongoose = require("mongoose");
 var config = require("../../deployment.config.json");
+
+var userSchema = mongoose.Schema({
+    firstName:{type:String, required:true},
+    lastName: {type:String, required:true},
+    emailAddress:{type:String,required:true},
+    userName:{type:String,required:false,unique:true},
+    password:{type:String, required:false}
+});
+
 module.exports.Init = function(){
     var connectionString = "mongodb://"+config.dbUserName+":"+config.dbPassword+"@"+config.dbHost+":"+config.dbPort+
     "/"+config.dbName;
--- sport-board/src/app-server/userApi.js
+++ sport-board/src/app-server/userApi.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var dbUtils = require("./dbUtil");
 var api = express.Router();
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({extended:true}));
 
 api.get("/user/:id",function(req,res){
     res.send("Server Ok");
@@ -11,7 +14,18 @@ api.get("/getusers",function(req,res){
     res.send({name:"vaibhav",email:"vaibhavpfatkal@gmail.com"})
 });
 
-api.post("/register",function(req,res){
+api.post("/login",function(req,res){
+    var postedData = req.body;
+    console.log(postedData);
+    if(postedData!=undefined){
+        console.log(postedData.userName);
+        console.log(postedData.password);
+        res.set(200);
+       res.end("success");
+    }else {
+        res.set({status:401});
+    res.send("failure");
+    }
     
 });
 
